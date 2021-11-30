62a35376854bfa19b11eadc94fcbd933d4e15cdf
sreehari2003 <sreeharivijaya2003@gmail.com>
2021-11-30 23:32:32 +0530

    new tool kit added

--- src/store/index.js
+++ src/store/index.js
@@ -1,26 +1,34 @@
-import { createStore, getState, subscribe } from "redux";
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 
-const initiaState = { showCounter: true, counter: 0 };
+const initialState = { counter: 0, showCounter: true };
 
-const counterReducer = (state = initiaState, action) => {
-  if (action.type === "add") {
-    return {
-      counter: state.counter + action.amount,
-      showCounter: state.showCounter,
-    };
-  } else if (action.type === "sub") {
-    return {
-      counter: state.counter - 1,
-      showCounter: state.showCounter,
-    };
-  } else if (action.type === "toggle") {
-    return {
-      counter: 0,
-      showCounter: state.showCounter ? false : true,
-    };
-  }
-  return state;
-};
-const store = createStore(counterReducer);
+const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.payload;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+      state.counter = 0;
+    },
+  },
+});
+
+const store = configureStore({
+  reducer: counterSlice.reducer,
+  //VIMP
+  ///here createSlice.reducer is not a typo it is object underhood
+});
+
+export const counterActions = counterSlice.actions;
+// //these are functions we declared we are passing the function to counter.js
 
 export default store;
